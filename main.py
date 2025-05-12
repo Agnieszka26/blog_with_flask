ed7b26a92dabef2a1063d00e725a8df7657601f0
Agnieszka26 <medrek.agn@yahoo.com>
2025-05-12 09:52:11 +0200

    day 60: send messages

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-from flask import Flask, render_template
+import smtplib
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+GMAIL_ADDRESS = os.getenv("GMAIL_ADDRESS")
+GMAIL_PASSWORD = os.getenv("GMAIL_PASSWORD")
+GMAIL_HOST_NAME = os.getenv("GMAIL_HOST_NAME")
+PORT = int(os.getenv("PORT"))
+TIMEOUT= int(os.getenv("TIMEOUT"))
+
+from flask import Flask, render_template, request
 import requests
 
 app = Flask(__name__)
@@ -18,6 +29,29 @@ def about():
 @app.route('/contact')
 def contact():
     return render_template("contact.html")
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+@app.route('/dashboard', methods=["post"])
+def dashboard():
+    username= "none"
+    password = "none"
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+    return render_template('dashboard.html', name=username, password=password)
+
+@app.route("/form-entry", methods=["post"])
+def receive_data():
+    message = request.form['message']
+    email = request.form['email']
+    with smtplib.SMTP(GMAIL_HOST_NAME, PORT, timeout=TIMEOUT) as connection:
+        connection.starttls()
+        connection.login(GMAIL_ADDRESS, GMAIL_PASSWORD)
+        connection.sendmail(from_addr=email, to_addrs=GMAIL_ADDRESS, msg=f'Subject: From your webpage \n\n {message}')
+    return render_template("message.html", message=message)
 
 @app.route("/post/<int:uuid>")
 def get_post(uuid):
