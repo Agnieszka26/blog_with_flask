bc20fcd69d45263cc6fd39cff67388d461f6b0e7
Agnieszka26 <medrek.agn@yahoo.com>
2025-05-20 11:48:08 +0200

    Be able to Get Blog Post items

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 import smtplib
+from flask import Flask, render_template, redirect, url_for
+
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Text
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, URL
+from flask_ckeditor import CKEditor, CKEditorField
+from datetime import date
+
 from dotenv import load_dotenv
 import os
 load_dotenv()
@@ -16,10 +27,35 @@ app = Flask(__name__)
 
 blog_url = 'https://api.npoint.io/3eba1fb773d75db0fe57'
 resp = requests.get(blog_url)
-posts = resp.json()
+p = resp.json()
+# CREATE DATABASE
+class Base(DeclarativeBase):
+    pass
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
+
+# CONFIGURE TABLE
+class BlogPost(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    subtitle: Mapped[str] = mapped_column(String(250), nullable=False)
+    date: Mapped[str] = mapped_column(String(250), nullable=False)
+    body: Mapped[str] = mapped_column(Text, nullable=False)
+    author: Mapped[str] = mapped_column(String(250), nullable=False)
+    img_url: Mapped[str] = mapped_column(String(250), nullable=False)
+
+
+with app.app_context():
+    db.create_all()
+
+
 
 @app.route('/')
 def home():
+    with app.app_context():
+        posts = db.session.execute(db.select(BlogPost).order_by(BlogPost.date)).scalars().all()
     return render_template("index.html", posts=posts)
 
 @app.route('/about')
@@ -53,13 +89,22 @@ def receive_data():
         connection.sendmail(from_addr=email, to_addrs=GMAIL_ADDRESS, msg=f'Subject: From your webpage \n\n {message}')
     return render_template("message.html", message=message)
 
+
+
 @app.route("/post/<int:uuid>")
 def get_post(uuid):
-    requested_post = None
-    for blog_post in posts:
-        if blog_post["id"] == uuid:
-            requested_post = blog_post
+    with app.app_context():
+        requested_post = db.session.execute(db.select(BlogPost).where(BlogPost.id == uuid)).scalar()
     return render_template('post.html', post=requested_post)
+
+# TODO: add_new_post() to create a new blog post
+
+# TODO: edit_post() to change an existing blog post
+
+# TODO: delete_post() to remove a blog post from the database
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
