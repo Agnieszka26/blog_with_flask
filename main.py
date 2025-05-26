954795af871e9f18a56373bceb71871d99af3d56
Agnieszka26 <medrek.agn@yahoo.com>
2025-05-26 16:12:46 +0200

    Creatin Relational Databases

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import smtplib
 from functools import wraps
+from typing import List
 from wsgiref.validate import validator
 from flask import Flask, render_template, redirect, url_for, flash, request
 from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import Integer, String, Text
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from sqlalchemy import Integer, String, Text, ForeignKey
 from flask_wtf import FlaskForm
 from werkzeug.security import generate_password_hash, check_password_hash
 from wtforms import StringField, SubmitField
@@ -45,23 +46,53 @@ db.init_app(app)
 class BlogPost(db.Model):
     __tablename__ = "blog_post"
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    # Create Foreign Key, "users.id" the users refers to the tablename of User.
+    author_id: Mapped[int] = mapped_column(Integer, db.ForeignKey("users.id"))
+    # Create reference to the User object. The "posts" refers to the posts property in the User class.
+    author = relationship("User", back_populates="posts")
+
     title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
     subtitle: Mapped[str] = mapped_column(String(250), nullable=False)
     date: Mapped[str] = mapped_column(String(250), nullable=False)
     body: Mapped[str] = mapped_column(Text, nullable=False)
-    author: Mapped[str] = mapped_column(String(250), nullable=False)
     img_url: Mapped[str] = mapped_column(String(250), nullable=False)
 
 
 class User(db.Model, UserMixin):
     __tablename__ = "users"
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    # This will act like a List of BlogPost objects attached to each User.
+    # The "author" refers to the author property in the BlogPost class.
     email: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(String(250), nullable=False)
     name: Mapped[str] = mapped_column(String(250), nullable=False)
+    posts = relationship("BlogPost", back_populates="author")
 
 with app.app_context():
     db.create_all()
+
+def get_author(post):
+    post_dict = post.__dict__.copy()
+    post_dict.pop('_sa_instance_state', None)
+    author = db.session.execute(db.select(User).where(User.id == post.author_id)).scalar()
+    post_dict["author_name"] = author.name
+    return post_dict
+
+def get_posts_with_author():
+    posts_arr = []
+    with app.app_context():
+        posts = db.session.execute(db.select(BlogPost).order_by(BlogPost.date)).scalars().all()
+        for post in posts:
+            print(post.author.name) #PROŚCIEJ Xd
+            post_dict = get_author(post)
+            posts_arr.append(post_dict)
+    return posts_arr
+
+def get_post_with_author(uuid):
+    with app.app_context():
+        requested_post = db.session.execute(db.select(BlogPost).where(BlogPost.id == uuid)).scalar()
+        post_dict = get_author(requested_post)
+    return post_dict
 
 def admin_only(f):
     @wraps(f)
@@ -77,9 +108,7 @@ def load_user(user_id):
 
 @app.route('/')
 def home():
-    with app.app_context():
-        posts = db.session.execute(db.select(BlogPost).order_by(BlogPost.date)).scalars().all()
-        print(posts)
+    posts = get_posts_with_author()
     return render_template("index.html", posts=posts, current_user=current_user)
 
 @app.route('/about')
@@ -100,7 +129,7 @@ def login():
         if user and check_password_hash(user.password, password):
             login_user(user)
             flash('You were successfully logged in')
-            return redirect("dashboard")
+            return redirect(url_for("dashboard"))
         else:
             flash('Invalid credentials')
 
@@ -137,7 +166,7 @@ def logout():
 def dashboard():
     return render_template('dashboard.html', current_user=current_user)
 
-@app.route("/form-entry", methods=["post"])
+@app.route("/form-entry", methods=["POST", "GET"])
 def receive_data():
     message = request.form['message']
     email = request.form['email']
@@ -149,8 +178,7 @@ def receive_data():
 
 @app.route("/post/<int:uuid>")
 def get_post(uuid):
-    with app.app_context():
-        requested_post = db.session.execute(db.select(BlogPost).where(BlogPost.id == uuid)).scalar()
+    requested_post = get_post_with_author(uuid)
     return render_template('post.html', post=requested_post, current_user=current_user)
 
 @app.route("/new-post", methods=["POST", "GET"])
@@ -171,7 +199,7 @@ def add_new_post():
         return redirect('/')
     return render_template("make-post.html", form=form, current_user=current_user)
 
-@app.route("/edit-post/<uuid>", methods=["POST", "GET"])
+@app.route("/edit-post/<int:uuid>", methods=["POST", "GET"])
 @admin_only
 def edit_post(uuid):
     edited_post = db.session.execute(db.select(BlogPost).where(BlogPost.id == uuid)).scalar()
@@ -190,7 +218,7 @@ def edit_post(uuid):
         return redirect('/')
     return render_template("make-post.html", form=form, current_user=current_user)
 
-@app.route('/delete/<uuid>')
+@app.route('/delete/<int:uuid>')
 @admin_only
 def delete_post(uuid):
     post = db.get_or_404(BlogPost, uuid)
